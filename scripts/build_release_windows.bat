1fd767e16140ef025bde19a1f4465b3d97884015
Kira Reyphia <referter0071@gmail.com>
2026-09-20 20:53:07 +0300

    Delete scripts directory

--- scripts/build_release_windows.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Double-click-friendly wrapper around build_release_windows.ps1 -- avoids
-REM users having to fight PowerShell's default execution policy just to run
-REM a build script.
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0build_release_windows.ps1" %*
-pause
